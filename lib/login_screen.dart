5365c1038ddedab29a6d068e2931d13dcf556692
s_erfani <saeederfani75@gmail.com>
2021-06-08 19:47:59 +0430

    sign in screen improvement

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -17,46 +17,83 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Container(
           decoration: BoxDecoration(color: Colors.lightBlue),
           child: Center(
-            child: Card(
-              margin: const EdgeInsets.all(32),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  margin: const EdgeInsets.all(32),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Sign in",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.mail_outline),
+                              labelText: "Email",
+                              border: InputBorder.none),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.lock_outline),
+                              labelText: "Password",
+                              border: InputBorder.none),
+                        ),
+                        const SizedBox(height: 8),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            'Forgot Password?',
+                            textAlign: TextAlign.end,
+                          ),
+                        ),
+                        CheckboxListTile(
+                            controlAffinity: ListTileControlAffinity.leading,
+                            contentPadding: const EdgeInsets.all(0),
+                            title: Text("Remember Me"),
+                            value: boxVal,
+                            onChanged: (val) {
+                              setState(() {
+                                boxVal = val!;
+                              });
+                            }),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(45.0),
+                            )),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 32, vertical: 8),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Sign in"),
-                    TextField(
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.mail_outline),
-                          labelText: "Email",
-                          border: InputBorder.none),
+                    Text('Don\'t have an Account?'),
+                    const SizedBox(width: 8),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                    TextField(
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.lock_outline),
-                          labelText: "Password",
-                          border: InputBorder.none),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        'Forgot Password?',
-                        textAlign: TextAlign.end,
-                      ),
-                    ),
-                    CheckboxListTile(
-                        controlAffinity: ListTileControlAffinity.leading,
-                        contentPadding: const EdgeInsets.all(0),
-                        title: Text("Remember Me"),
-                        value: boxVal,
-                        onChanged: (val) {
-                          setState(() {
-                            boxVal = val!;
-                          });
-                        }),
                   ],
                 ),
-              ),
+              ],
             ),
           ),
         ),
