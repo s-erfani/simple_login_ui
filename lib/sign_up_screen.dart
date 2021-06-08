1c4bdc05c5cacb5035011085810502242d2b6ac2
s_erfani <saeederfani75@gmail.com>
2021-06-08 20:18:45 +0430

    minor

--- lib/sign_up_screen.dart
+++ lib/sign_up_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_login_ui/login_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -17,83 +18,94 @@ class _SignUpScreenState extends State<SignUpScreen> {
         body: Container(
           decoration: BoxDecoration(color: Colors.lightBlue),
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Card(
-                  margin: const EdgeInsets.all(32),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          "Sign UP",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                              icon: Icon(Icons.perm_identity_outlined),
-                              labelText: "Full Name",
-                              border: InputBorder.none),
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                              icon: Icon(Icons.phone_iphone_outlined),
-                              labelText: "Phone Number",
-                              border: InputBorder.none),
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                              icon: Icon(Icons.mail_outline),
-                              labelText: "Email",
-                              border: InputBorder.none),
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                              icon: Icon(Icons.lock_outline),
-                              labelText: "Password",
-                              border: InputBorder.none),
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                              icon: Icon(Icons.lock_outline),
-                              labelText: "Confirm Password",
-                              border: InputBorder.none),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(45.0),
-                            )),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    margin: const EdgeInsets.all(32),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "Sign UP",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 32, vertical: 8),
-                            child: Text(
-                              'Register',
-                              style: TextStyle(fontSize: 24),
+                          TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.perm_identity_outlined),
+                                labelText: "Full Name",
+                                border: InputBorder.none),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.phone_iphone_outlined),
+                                labelText: "Phone Number",
+                                border: InputBorder.none),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.mail_outline),
+                                labelText: "Email",
+                                border: InputBorder.none),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.lock_outline),
+                                labelText: "Password",
+                                border: InputBorder.none),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.lock_outline),
+                                labelText: "Confirm Password",
+                                border: InputBorder.none),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(45.0),
+                              )),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 32, vertical: 8),
+                              child: Text(
+                                'Register',
+                                style: TextStyle(fontSize: 24),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Have an Account?'),
-                    const SizedBox(width: 8),
-                    Text(
-                      'Sign in',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Have an Account?'),
+                      const SizedBox(width: 8),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (ctx) => LoginScreen()));
+                        },
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
